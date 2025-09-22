b2ccc5b5cd89b860f32b656d3a71ae30a758b65f
Ahmed ZAghloul <zaghloul85@gmail.com>
2025-09-22 15:59:31 +0300

    debugging api call

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,20 +5,21 @@ import { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   // 1. Get the auth token from the request cookies
   const token = request.cookies.get('auth_token');
-
+  console.log('token in middleware ', token);
+  
   const { pathname } = request.nextUrl;
   const loginUrl = new URL('/auth/google', request.url);
   const dashboardUrl = new URL('/dashboard', request.url);
 
   // 2. If the user is trying to access the dashboard without a token,
-  //    redirect them to the login page.
-  // if (!token && pathname.startsWith('/dashboard')) {
-  //   return NextResponse.redirect(loginUrl);
-  // }
+    //  redirect them to the login page.
+  if (!token && pathname.startsWith('/dashboard')) {
+    return NextResponse.redirect(loginUrl);
+  }
 
-  // if (token && pathname.startsWith('/auth/google')) {
-  //   return NextResponse.redirect(dashboardUrl)
-  // }
+  if (token && pathname.startsWith('/auth/google')) {
+    return NextResponse.redirect(dashboardUrl)
+  }
 
   // 4. Otherwise, continue with the request
   return NextResponse.next();
--- src/services/api/auth.ts
+++ src/services/api/auth.ts
@@ -2,7 +2,9 @@ import api from '@/src/lib/api';
 import { User } from '@/src/types/auth'; // We'll create this type next
 
 export const getProfile = async (): Promise<{user:User, csrfToken: string}> => {
+  
   const { data } = await api.get('/auth/profile');
+    console.log('get profile called ', data);
   return data;
 };
 
