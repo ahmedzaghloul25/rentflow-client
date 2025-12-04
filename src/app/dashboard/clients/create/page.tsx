441513735b7f385befc7cdd7285be8501dddebdd
Ahmed ZAghloul <zaghloul85@gmail.com>
2025-12-04 11:41:08 +0200

    update client/create

--- src/app/dashboard/clients/create/page.tsx
+++ src/app/dashboard/clients/create/page.tsx
@@ -3,7 +3,7 @@
 import { ClientForm } from '@/src/components/forms/ClientForm';
 import { Box, Paper, Typography } from '@mui/material';
 
-export default function CreateClientPage() {
+export default async function CreateClientPage() {
   return (
     <Box>
       <Typography variant="h4" component="h1" sx={{ mb: 3 }}>
