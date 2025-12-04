ba7e6ec2ff2de35cbca524d899df37ecbcc488a1
Ahmed ZAghloul <zaghloul85@gmail.com>
2025-12-04 11:32:11 +0200

    update dependencies

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  //  async rewrites() {
-  //   return [
-  //     {
-  //       source: '/api/:path*',
-  //       destination: 'http://localhost:3100/:path*', // Proxy to your local NestJS backend
-  //     },
-  //   ];
-  // },
-  // allowedDevOrigins: ['d6225cb0791c.ngrok-free.app to /_next/*']
 };
 
 export default nextConfig;
